f537507dadcb29b88fcf2a3317d430061325b4a9
fernandourzua <fernandourzua@pop-os.localdomain>
2023-11-16 13:58:55 -0300

    working

--- 268/sol/MissingNumber.go
+++ 268/sol/MissingNumber.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{1, 2, 3, 4, 6}
+	arr := []int{1, 2, 3, 4, 5, 6, 8, 7, 9, 10, 12, 11, 14}
 	res := missingNumber(arr)
 	fmt.Println("El numero que falta en el array es: ", res)
 }
@@ -11,7 +11,7 @@ func main() {
 func missingNumber(arr []int) int {
 	//we need to return the only number that´s not there,
 	//so we sum the number from 1 to len(arr) ant then substract the sum of the array
-	var arrlen int = len(arr)
+	var arrlen int = len(arr) + 1
 	var total int = (arrlen * (arrlen + 1)) / 2
 	var arrsum int = 0
 	for i := 0; i < len(arr); i++ {
