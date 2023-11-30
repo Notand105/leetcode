b8f9a9c93e98fc725fbcbe8872e4fe396934b933
Notand105 <urzua.fernando.f@gmail.com>
2023-11-30 07:58:11 -0300

    new

--- 21/sol/main.go
+++ 21/sol/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 
 	list1 := []int{1, 3, 5, 7, 9, 11, 12, 15, 19} //9
-	list2 := []int{2, 4, 6, 9, 9, 9, 9, 10}       // 8
+	list2 := []int{-9, -4, 2, 6, 9, 9, 9, 10}     // 8
 
 	sol := mergeArr(list1, list2)
 	fmt.Println(sol)
@@ -18,23 +18,24 @@ func mergeArr(list1, list2 []int) []int {
 	//ver si existe alguna forma de OOP ver senio go en youtube para ver que ral
 
 	aux := []int{}
-	j := 0
-	for i := 0; i < len(list1) || j < len(list2); i++ {
-		if i >= len(list1) && j < len(list2) {
-			i--
-		} else if j >= len(list2) && i < len(list1) {
-			j--
-		}
 
-		fmt.Printf("{ i: %d , j: %d , list1[i]: %d, list2[j]: %d } \n", i, j, list1[i], list2[j])
-		if list1[i] < list2[j] {
-			aux = append(aux, list1[i])
-			fmt.Printf("agregamos %d \n", list1[i])
+	if len(list1) == 0 {
+		return list2
+	}
+	if len(list2) == 0 {
+		return list1
+	}
+	fmt.Println(list1, list2)
+	for len(list1) > 0 {
+		if len(list2) == 0 {
+			list2 = append(list2, 101)
+		} //los  numeros pueden estar entre -100 y 100, por eso si esta vacia l2 se continua el ciclo sin interferir
+		if list1[0] < list2[0] {
+			aux = append(aux, list1[0])
+			list1 = list1[1:]
 		} else {
-			aux = append(aux, list2[j])
-			fmt.Printf("agregamos %d \n", list2[j])
-			j++
-			i--
+			aux = append(aux, list2[0])
+			list2 = list2[1:]
 		}
 	}
 
